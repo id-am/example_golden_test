d7c841ab779a9a46fc2891d3fccc35d3748ab801
Ivan Dario Avila Martinez <idamkiller@hotmail.com>
2025-07-05 10:18:57 -0500

    feat: add ButtonData model for managing button properties

---
+++ lib/models/button_data.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ButtonData {
+  final String description;
+  final Color color;
+
+  const ButtonData({required this.description, required this.color});
+}
